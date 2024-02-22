884efcb54c5155ac809ab0e5d7edf079d0eb2d40
Abbey <habbey@Abbeys-MacBook-Pro.local>
2024-02-22 17:42:12 +0100

    Prottect /app routes

--- src/components/Auth/LoginForm.tsx
+++ src/components/Auth/LoginForm.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
 	Stack,
 	Input,
@@ -11,7 +11,7 @@ import {
 	Snackbar,
 } from '@mui/joy';
 import { useRouter } from 'next/navigation';
-import { signIn } from 'next-auth/react';
+import { signIn, signOut } from 'next-auth/react';
 import { isEmailCorrect } from '@/lib/form';
 
 const LoginForm = () => {
